76db5881965b43152a0290e8f5d7e0ffd91ccc39
Thiyagu <thiyagu@thoughtworks.com>
2011-06-26 11:34:58 +0530

    generated skeleton app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trendz::Application.config.secret_token = '06ba2b32915b6b16e992000cc8e77953b09d628e856747abf52b00fe6af1196f57f84b6ca72f94826e8f05a20045934a7ca075a9ebd63c9d677ce2c28e38f61a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Trendz::Application.config.session_store :cookie_store, :key => '_trendz_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Trendz::Application.config.session_store :active_record_store
