76115f8b0234c299584e792bce39f183a7c86447
Thiyagu P <thiyagu@gmail.com>
2011-09-15 09:03:20 +0530

    UI Changes

--- app/controllers/fo_charts_controller.rb
+++ app/controllers/fo_charts_controller.rb
@@ -14,7 +14,7 @@ class FoChartsController < ApplicationController
 
   def show_ratio
     @stock = Stock.find_by_symbol(params[:symbol])
-    start_date = Date.today - 365
+    start_date = Date.today - 90
     @ratio_data = FoQuote.connection.select_all("select date, sum(case when fo_type like 'C%' then open_interest else 0 end) as total_call_oi, sum(case when fo_type like 'P%' then open_interest else 0 end) as total_put_oi, sum(case when fo_type like 'C%' then traded_quantity else 0 end) as total_call, sum(case when fo_type like 'P%' then traded_quantity else 0 end) as total_put from fo_quotes where stock_id = #{@stock.id} and date >= '#{start_date}' and fo_type <> 'XX' and expiry_series in ('current', 'next') group by date order by date")
     quotes = EqQuote.find_all_by_stock_id(@stock.id, :order => 'date', :conditions => "date >= '#{start_date}'")
     @price_movement = quotes.collect do |quote|
