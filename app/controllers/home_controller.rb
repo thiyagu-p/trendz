44123ec28f922e7b6a6a33699b5f774d5f135596
Thiyagu P <thiyagu@gmail.com>
2013-02-10 22:57:10 +0530

    Portfolio view to show YTD P&L

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,5 +6,6 @@ class HomeController < ApplicationController
     @face_value_actions = FaceValueAction.where("ex_date >= ?" , Date.today).order(:ex_date)
     @action_errors = CorporateActionError.where("ex_date >= ? and not is_ignored" , Date.today).order(:ex_date)
     @equity_holding = EquityHolding.all
+    @year_beginning = EqQuote.where("date >= ?", Date.today.beginning_of_year).minimum(:date)
   end
 end
