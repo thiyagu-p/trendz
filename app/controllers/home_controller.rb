c3a8a865c7dc3f6fd6b56c9a0c94b04f4a25a48f
Thiyagu P <thiyagu@gmail.com>
2013-12-29 22:45:03 +0100

    displaying consolidated data

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,10 +2,32 @@ class HomeController < ApplicationController
 
   def index
     @dividend_actions = DividendAction.future_dividends_with_current_percentage
-    @bonus_actions = BonusAction.where("ex_date >= ?" , Date.today).order(:ex_date)
-    @face_value_actions = FaceValueAction.where("ex_date >= ?" , Date.today).order(:ex_date)
-    @action_errors = CorporateActionError.where("ex_date >= ? and not is_ignored" , Date.today).order(:ex_date)
-    @equity_holding = EquityHolding.all
+    @bonus_actions = BonusAction.where("ex_date >= ?", Date.today).order(:ex_date)
+    @face_value_actions = FaceValueAction.where("ex_date >= ?", Date.today).order(:ex_date)
+    @action_errors = CorporateActionError.where("ex_date >= ? and not is_ignored", Date.today).order(:ex_date)
+    @consolidated_holdings = consolidate_holding EquityHolding.all
     @year_beginning = EqQuote.where("date >= ?", Date.today.beginning_of_year).minimum(:date)
+  end
+
+  private
+
+  def consolidate_holding(holdings)
+    holdings = holdings.group_by { |h| h.equity_transaction.stock }
+    hash = {}
+    today = Date.today
+    holdings.each_pair do |stock, holding_list|
+      tot_qty = tot_cost = avg_days = 0
+      holding_list.each do |holding|
+        tot_qty += holding.quantity
+        tot_cost += holding.quantity * holding.equity_transaction.price
+        avg_days += today - holding.equity_transaction.date
+      end
+      hash[stock] = {
+          quantity: tot_qty,
+          average_cost: (tot_cost / tot_qty).round(2),
+          average_days: (avg_days / holding_list.size).round(0)
+      }
+    end
+    hash
   end
 end
--- lib/equity/corporate_action_performer.rb
+++ lib/equity/corporate_action_performer.rb
@@ -1,10 +1,12 @@
-class CorporateActionPerformer
+module Equity
 
-  def self.perform
-    actions_to_apply = "not applied and ex_date <= '#{Date.today}'"
-    [BonusAction, FaceValueAction, DividendAction].each do |action_type|
-      actions = action_type.send(:where, actions_to_apply).send(:to_a)
-      actions.each {|action| action.apply}
+  class CorporateActionPerformer
+    def self.perform
+      actions_to_apply = "not applied and ex_date <= '#{Date.today}'"
+      [FaceValueAction, BonusAction, DividendAction].each do |action_type|
+        actions = action_type.send(:where, actions_to_apply).send(:to_a)
+        actions.each { |action| action.apply }
+      end
     end
   end
 end
